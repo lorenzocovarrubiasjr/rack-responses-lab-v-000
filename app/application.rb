fefb7a95e50c87d2b16c7d4d531ce51d64ea885d
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-29 19:19:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     if 
-      write 
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end 
     
   end 
   
